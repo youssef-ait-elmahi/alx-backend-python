bdc47accdb45932dca67431d4ce9295226f5e69c
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-01-11 17:23:07 +0100

    Refactor zoom_array function to accept a list of integers instead of a tuple and remove unnecessary type annotations

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,26 +4,13 @@ This module contains a function that returns a list with each
 element of the input list repeated a number of times.
 """
 
-from typing import List, Tuple, Union
+from typing import List
 
 
-def zoom_array(lst: Tuple[Union[int,
-               float]], factor: int = 2) -> List[Union[int, float]]:
-    """
-    Function to return a list with each element of the input
-    list repeated a number of times.
-
-    Parameters:
-    lst (Tuple[Union[int, float]]): The tuple of integers and/or floats.
-    factor (int): The number of times each element should be repeated.
-
-    Returns:
-    List[Union[int, float]]: The list with each element of the input
-    list repeated a number of times.
-    """
-
-    zoomed_in: List[Union[int, float]] = [
+def zoom_array(lst: List[int], factor: int = 2) -> List[int]:
+    zoomed_in: List[int] = [
         item for item in lst
         for i in range(factor)
     ]
+
     return zoomed_in
