c04c039d56bbac9293b803a09281846f788d41ad
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-01-11 17:18:24 +0100

    Refactor type_checking.py module

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
-This module contains a function that returns
-a list with each element of the input list repeated a number of times.
+This module contains a function that returns a list with each
+element of the input list repeated a number of times.
 """
 
 from typing import List, Tuple, Union
@@ -10,17 +10,16 @@ from typing import List, Tuple, Union
 def zoom_array(lst: Tuple[Union[int,
                float]], factor: int = 2) -> List[Union[int, float]]:
     """
-    Function to return a list with each element of the
-    input list repeated a number of times.
+    Function to return a list with each element of the input
+    list repeated a number of times.
 
     Parameters:
-    lst (Tuple[Union[int, float]]): The tuple of integers
-    and/or floats.
+    lst (Tuple[Union[int, float]]): The tuple of integers and/or floats.
     factor (int): The number of times each element should be repeated.
 
     Returns:
-    List[Union[int, float]]: The list with each element of the
-    input list repeated a number of times.
+    List[Union[int, float]]: The list with each element of the input
+    list repeated a number of times.
     """
 
     zoomed_in: List[Union[int, float]] = [
