5d493710baae4a9f3bc8dcf9caea7b3d427664ae
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-01-11 17:49:36 +0100

    Update type annotations in zoom_array function

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,13 +4,12 @@ This module contains a function that returns a list with each
 element of the input list repeated a number of times.
 """
 
-from typing import List
+from typing import List, Tuple
 
 
-def zoom_array(lst: List[int], factor: int = 2) -> List[int]:
-    zoomed_in: List[int] = [
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
-
     return zoomed_in
