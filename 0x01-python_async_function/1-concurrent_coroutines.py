f2d82ca8d2657f2153dbdad3431e8aab2518464a
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-01-16 01:04:07 +0100

    Fix import statement in concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import asyncio
 from typing import List
-from 0-basic_async_syntax import wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     tasks = [wait_random(max_delay) for _ in range(n)]
