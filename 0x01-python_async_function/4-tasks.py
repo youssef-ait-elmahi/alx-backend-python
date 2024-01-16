86a54159ddea9f1fa6952f03bde3b091a5b687c9
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-01-16 01:23:02 +0100

    Add task_wait_n function to asynchronously wait for n random delays

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+async routine called task_wait_n that takes in 2 int arguments
+"""
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Asynchronous coroutine that waits for n random delays
+    and returns them in ascending order.
+
+    Args:
+        n (int): The number of random delays.
+        max_delay (int): The maximum delay value.
+
+    Returns:
+        List[float]: A list of delay values in ascending order.
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = []
+    for future in asyncio.as_completed(tasks):
+        delay = await future
+        delays.append(delay)
+    return delays
